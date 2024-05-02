dffaa30d2125f35fd18af9beb014746bfcddd6d7
Manohar Reddy <manoharreddychiluka1516@gmail.com>
2024-05-02 11:55:19 -0500

    first commit

--- src/Forclass1.java
+++ src/Forclass1.java
@@ -1,19 +1,45 @@
 public class Forclass1 {
 
     public void class1(){
-
+        for(int i=0; i<10; i++ )
+        {
+            System.out.print(i);
+            System.out.print(" ");
+        }
+        System.out.println();
     }
 
     public void class2(){
-
+        for(int i=0; i<10; i++)
+        {
+            for(int j=0; j<10; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 
     public void class3(){
-
+        for(int i=0; i<=10; i++)
+        {
+            for(int j=0; j<i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 
     public void class4(){
-
+        for(int i=10; i>=0; i--)
+        {
+            for(int j=i; j>=0; j--)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 
     public void class5(){
